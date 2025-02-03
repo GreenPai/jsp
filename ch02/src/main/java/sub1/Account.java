3ab14e202b923e243f0299774ffb1a4574380c5e
roberty <clsrntkdgh@gmail.com>
2025-02-03 11:55:19 +0900

    JSP 문법, 지시자

--- ch02/src/main/java/sub1/Account.java
+++ ch02/src/main/java/sub1/Account.java
@@ -1,5 +1,41 @@
 package sub1;
 
-public class Account {
+import java.io.IOException;
 
+import jakarta.servlet.jsp.JspWriter;
+
+public class Account {
+	
+	private String bank;
+	private String id;
+	private String name;
+	private int balance;
+	
+	// 생성자
+	public Account(String bank, String id, String name, int balance) {
+		super();
+		this.bank = bank;
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+	
+	public void deposit(int money) {
+		this.balance += money;
+	}
+	
+	public void withdraw(int money) {
+		this.balance -= money;
+	}
+	
+	public void show(JspWriter out) throws IOException {
+		out.println("<p>");
+		out.println("은행명 : "  + this.bank + "<br>");
+		out.println("계좌번호 : " + this.id  + "<br>");
+		out.println("입금주 : "  + this.name + "<br>");
+		out.println("현재잔액 : " + this.balance + "<br>");
+		out.println("</p>");
+	}
+	
+	
 }
